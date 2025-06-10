a49105cbffac96ba33f8b032998923100d871c03
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-06-10 11:33:52 +0530

    Developed Fully Functional AllItem,CreateItem,HomeScreen and LowStock Screens

--- src/screens/CreateItems.jsx
+++ src/screens/CreateItems.jsx
@@ -1,12 +1,158 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Pressable,
+  Text,
+  FlatListComponent,
+  FlatList,
+} from 'react-native';
 
-const CreateItems = () => {
+const CreateItems = ({data, setData}) => {
+  const [itemName, setItemName] = useState('');
+  const [stockAmount, setStockAmount] = useState('');
+  const [isEdit, setIsEdit] = useState(false);
+  const [EditItemId, setEditItemId] = useState(null);
+
+  const addItemHandler = () => {
+    const newItem = {
+      id: Date.now(),
+      name: itemName,
+      stock: stockAmount,
+    };
+    setData([...data, newItem]);
+    setItemName('');
+    setStockAmount('');
+    setIsEdit(false);
+  };
+
+  const deleteItemHandler = id => {
+    setData(data.filter(item => item.id !== id));
+  };
+
+  const EditItemHandler = item => {
+    setIsEdit(true);
+    setItemName(item.name);
+    setEditItemId(item.id);
+    setStockAmount(item.stockAmount);
+  };
+
+  const updateItemHandler = () => {
+    setData(
+      data.map(item =>
+        item.id === EditItemId
+          ? {...item, name: itemName, stock: stockAmount}
+          : item,
+      ),
+    );
+    setItemName('');
+    setStockAmount('');
+  };
+
   return (
-    <View>
-      <Text>CreateItems</Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Enter an name..."
+        placeholderTextColor="#999"
+        style={styles.input}
+        value={itemName}
+        onChangeText={item => setItemName(item)}
+      />
+      <TextInput
+        placeholder="Enter stock amount..."
+        placeholderTextColor="#999"
+        style={styles.input}
+        value={stockAmount}
+        onChangeText={item => setStockAmount(item)}
+      />
+      <Pressable
+        style={styles.addButton}
+        onPress={() => (isEdit ? updateItemHandler() : addItemHandler())}>
+        <Text style={styles.buttonText}>
+          {isEdit ? 'Edit Item in Stock' : 'Add Item in Stock'}
+        </Text>
+      </Pressable>
+      <View>
+        <View style={styles.headingContainer}>
+          <Text style={styles.headingText}>All items in the Stock</Text>
+        </View>
+        <FlatList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <View
+              style={[
+                styles.itemContainer,
+                {backgroundColor: item.stock < 20 ? '#FFCCCC' : '#D7F68F'},
+              ]}>
+              <Text style={styles.itemText}>{item.name}</Text>
+              <View style={{flexDirection: 'row', gap: 10}}>
+                <Text style={styles.itemText}>{item.stock}</Text>
+                <Pressable onPress={() => EditItemHandler(item)}>
+                  <Text style={styles.itemText}>Edit</Text>
+                </Pressable>
+                <Pressable onPress={() => deleteItemHandler(item.id)}>
+                  <Text style={styles.itemText}>Delete</Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
+          contentContainerStyle={{gap: 10}}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: '4%',
+    gap: '10',
+  },
+  input: {
+    borderWidth: 1.5,
+    borderColor: 'green',
+    paddingHorizontal: 15,
+    paddingVertical: 18,
+    borderRadius: 7,
+  },
+  addButton: {
+    backgroundColor: '#CABFEEFF',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  headingText: {
+    fontWeight: '500',
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 7,
+  },
+  itemText: {
+    fontWeight: '400',
+    fontSize: 15,
+  },
+  headingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+});
 
 export default CreateItems;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,62 +1,118 @@
-import React from 'react';
-import { StyleSheet,Text,View,Pressable,useState } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Pressable} from 'react-native';
 import AllItems from './AllItems';
 import LowStocks from './LowStocks';
 import CreateItems from './CreateItems';
 
+
 const HomeScreen = () => {
-    const [view,setView] = useState(0);
-    return(
-        <View style={styles.container}>
-            <Text style = {styles.title}>HomeScreen</Text>
-            <View style={styles.buttonContainer}>
-                <Pressable onPress={()=>setView(0)}>
-                    <Text style = {styles.button}>All Items</Text>
-                </Pressable>
-                <Pressable onPress={()=>setView(2)}>
-                    <Text style = {styles.button}>Low Stocks</Text>
-                </Pressable>
-                <Pressable onPress={()=>setView(3)}>
-                    <Text style = {styles.button}>Create Items</Text>
-                </Pressable>
-                {view === 0 && <AllItems/>}
-                {view === 2 && <LowStocks/>}
-                {view === 3 && <CreateItems/>}
-            </View>
-        </View>
-    );
+  const [view, setView] = useState(0);
+  const [data , setData] = useState([
+  {
+    id: 1,
+    name: 'wheat',
+    stock: 5,
+    unit: 'kg',
+  },
+  {
+    id: 2,
+    name: 'Rice',
+    stock: 55,
+    unit: 'kg',
+  },
+  {
+    id: 3,
+    name: 'Sugar',
+    stock: 25,
+    unit: 'kg',
+  },
+  {
+    id: 4,
+    name: 'Onion',
+    stock: 105,
+    unit: 'kg',
+  },
+  {
+    id: 5,
+    name: 'Ginger',
+    stock: 2,
+    unit: 'kg',
+  },
+]);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>HomeScreen</Text>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          style={[
+            styles.button,
+            view === 0 ? {backgroundColor: 'green'} : null,
+          ]}
+          onPress={() => setView(0)}>
+          <Text
+            style={[styles.buttonText, view === 0 ? {color: 'white'} : null]}>
+            All Items
+          </Text>
+        </Pressable>
+        <Pressable
+          style={[
+            styles.button,
+            view === 2 ? {backgroundColor: 'green'} : null,
+          ]}
+          onPress={() => setView(2)}>
+          <Text
+            style={[styles.buttonText, view === 2 ? {color: 'white'} : null]}>
+            Low Stocks
+          </Text>
+        </Pressable>
+        <Pressable
+          style={[
+            styles.button,
+            view === 3 ? {backgroundColor: 'green'} : null,
+          ]}
+          onPress={() => setView(3)}>
+          <Text
+            style={[styles.buttonText, view === 3 ? {color: 'white'} : null]}>
+            Create Items
+          </Text>
+        </Pressable>
+      </View>
+      {view === 0 && <AllItems data={data} />}
+      {view === 2 && <LowStocks data={data.filter((item) => item.stock <= 5)} />}
+      {view === 3 && <CreateItems data = {data} setData={setData}/>}
+    </View>
+  );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        width:'100%',
-        height:'100%',
-        padding:'4%',
-        backgroundColor:'#ffffff',
-    },
-    title:{
-        fontSIze:24,
-        fontWeight:'bold',
-        color:'#333',
-    },
-    buttonContainer:{
-        flexDirection :'row',
-        gap:14,
-        paddingVertical:5,
-    },
-    button:{
-        paddingVertical:5,
-        paddingHorizontal:10,
-        borderRadius:10,
-        borderWidth:1,
-        borderColor:'green',
-    },
-    buttonText:{
-        color:'green',
-        fontWeight:'400',
-        fontSize:12,
-    },
-
+  container: {
+    width: '100%',
+    height: '100%',
+    padding: '4%',
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSIze: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    gap: 14,
+    paddingVertical: 5,
+  },
+  button: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'green',
+  },
+  buttonText: {
+    color: 'green',
+    fontWeight: '400',
+    fontSize: 12,
+  },
 });
